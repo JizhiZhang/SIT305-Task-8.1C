53edb266c83ce1c296726f214d0f6785c4a92f71
JIZHI ZHANG <69889275+JizhiZhang@users.noreply.github.com>
2021-05-15 23:10:32 +0800

    Upload files

---
+++ YouTubePlayerApp/app/src/main/java/com/example/pracyoutube/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.pracyoutube;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button playButton;
+    EditText urlEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        playButton = findViewById(R.id.playButton);
+        urlEditText = findViewById(R.id.urlEditText);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String url = urlEditText.getText().toString();
+                String playUrl;
+                if (url.length() != 0) {
+                    Intent intent = new Intent(MainActivity.this, VideoActivity.class);
+                    playUrl = url.substring(30);
+                    intent.putExtra("URL", playUrl);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, "Please enter a youtube url!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
---
+++ YouTubePlayerApp/app/src/main/java/com/example/pracyoutube/VideoActivity.java
@@ -0,0 +1,41 @@
+package com.example.pracyoutube;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class VideoActivity extends YouTubeBaseActivity {
+    YouTubePlayerView youTubePlayerView;
+    YouTubePlayer.OnInitializedListener onInitializedListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+        youTubePlayerView = findViewById(R.id.youtubeplayer);
+
+        Intent intent = getIntent();
+        String link = intent.getStringExtra("URL");
+
+        onInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo(link);
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+            }
+        };
+        youTubePlayerView.initialize(YoutubeConfig.getApiKey(), onInitializedListener);
+    }
+}
