d2e0c19ebea8fe399ad13c37344bff10873d1cbd
JACKZZ <69889275+JizhiZhang@users.noreply.github.com>
2021-06-04 15:25:08 +0800

    Delete Google Youtube player API KEY

--- YouTubePlayerApp/app/src/main/java/com/example/pracyoutube/YoutubeConfig.java
+++ YouTubePlayerApp/app/src/main/java/com/example/pracyoutube/YoutubeConfig.java
@@ -4,7 +4,7 @@ public class YoutubeConfig {
     public YoutubeConfig() {
     }
 
-    public static final String API_KEY = "AIzaSyDVKuc5wd3x3PhNOlefxr45YFUdiWivhv0";
+    public static final String API_KEY = "";
 
     public static String getApiKey(){
         return API_KEY;
